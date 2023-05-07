756e4974a61fc6ed1584c6cfeddfe44945ec0b91
Gustavo Miller Santos <gustavo.miller@quintoandar.com.br>
2023-05-07 17:31:36 -0300

    feat: add RunPythonScriptOperator

---
+++ orchestration/plugins/environment_operators/run_python_script.py
@@ -0,0 +1,25 @@
+from airflow.models.baseoperator import BaseOperator
+from environment_operators.strategies import EnvironmentStrategyEnum, EnvironmentStrategyFactory
+
+class RunPythonScriptOperator(BaseOperator):
+    template_fields = ['strategy_args', 'script_path']
+
+    def __init__(
+            self,
+            strategy: EnvironmentStrategyEnum,
+            strategy_args: dict,
+            script_path: str,
+            script_args: list = None,
+            **kwargs
+        ) -> None:
+        super().__init__(**kwargs)
+        
+        self.factory = EnvironmentStrategyFactory()
+        self.strategy_type = strategy
+        self.strategy_args = strategy_args
+        self.script_path = script_path
+        self.script_args = script_args
+
+    def execute(self, context):
+        strategy = self.factory.create(self.strategy_type, **self.strategy_args)
+        strategy.run_python_script(self.script_path, self.script_args)
